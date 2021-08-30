b51e80c07670acdd7f7c77ceeb5591d500e03ed6
subashinie <87689475+subashinie@users.noreply.github.com>
2021-08-30 23:03:54 +0530

    Add files via upload

---
+++ positive num.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 22:49:34 2021
+
+@author: suba
+"""
+
+# Python program to print positive Numbers in a List
+
+
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num >= 0:
+        print
+        print("\n")
+        print(num, end = " ")
+               
+list2=[12,14,-95,3]
+for num in list2:
+    if num>=0:
+        print("\n")
+        print(num,  end=" ")
+
+
+	
+	
+	
+	
